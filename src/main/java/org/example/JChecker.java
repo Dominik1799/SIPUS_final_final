67c84c49319506478b685cfa8f5d9137ea951c06
YKB <jakubhlavacka123@gmail.com>
2022-11-30 10:26:28 +0100

    altered TODO according to XADES_ZEP specification, required consulting

--- src/main/java/org/example/JChecker.java
+++ src/main/java/org/example/JChecker.java
@@ -19,6 +19,7 @@ public class JChecker implements Checker{
         checkDsSignatureValue();
     }
 
+    // specifikacia XADESU strana 17
     private void checkDsSignature() throws InvalidDocumentException {
         String REQUIRED_NAMESPACE_PREFIX = "ds";
         String EXPECTED_DS_NAMESPACE_URI = "http://www.w3.org/2000/09/xmldsig#";
@@ -28,22 +29,22 @@ public class JChecker implements Checker{
         if (dsSignature == null) {
             throw new InvalidDocumentException("Chýba ds:Signature element");
         }
+
         Attribute dsSignatureId = dsSignature.attribute("Id");
-        // TODO: can be blank???
+        // TODO: Id - ds:Signature element je referencovaný z elementu xades:QualifyingProperties, atribút Target(check reference)???
         // tested on documents 03(valid) and 05(invalid)
         if (dsSignatureId == null || dsSignatureId.getValue().equals("")) {
             throw new InvalidDocumentException("ds:Signature musí mať Id atribút");
         }
 
-
         Namespace xmlnsDs = dsSignature.getNamespaceForPrefix(REQUIRED_NAMESPACE_PREFIX);
-        // TODO: required URI check???
         // tested on documents 03(valid), missing not valid document according to this rule
         if (xmlnsDs == null || !xmlnsDs.getURI().equals(EXPECTED_DS_NAMESPACE_URI)) {
             throw new InvalidDocumentException("ds:Signature musí mať špecifikovaný namespace xmlns:ds");
         }
     }
 
+    // specifikacia XADESU strana 24
     private void checkDsSignatureValue() throws InvalidDocumentException {
         Element root = this.document.getRootElement();
         Element dsSignatureValue = (Element) root.selectSingleNode("//*[name() = 'ds:SignatureValue']");
@@ -53,7 +54,7 @@ public class JChecker implements Checker{
         }
 
         Attribute dsSignatureValueId = dsSignatureValue.attribute("Id");
-        // TODO: can be blank???
+        // TODO: Id – element ds:SignatureValue je referencovaný z elementu xades:SignatureTimeStamp(check reference)???
         // tested on documents 03(valid) and 06(invalid)
         if (dsSignatureValueId == null || dsSignatureValueId.getValue().equals("")) {
            throw new InvalidDocumentException("ds:SignatureValue – musí mať Id atribút");
