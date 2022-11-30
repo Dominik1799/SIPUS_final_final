a9dc12df0b9a486739431daba79ab04c78cf4732
YKB <jakubhlavacka123@gmail.com>
2022-11-30 10:17:29 +0100

    checking ds:SignatureValue attribute Id

--- src/main/java/org/example/JChecker.java
+++ src/main/java/org/example/JChecker.java
@@ -16,7 +16,7 @@ public class JChecker implements Checker{
     @Override
     public void startCheck() throws InvalidDocumentException {
         checkDsSignature();
-        // checkDsSignatureValue();
+        checkDsSignatureValue();
     }
 
     private void checkDsSignature() throws InvalidDocumentException {
@@ -26,10 +26,11 @@ public class JChecker implements Checker{
         Element root = this.document.getRootElement();
         Element dsSignature = (Element) root.selectSingleNode("//*[name() = 'ds:Signature']");
         if (dsSignature == null) {
-            throw new InvalidDocumentException("Chýba ds:Signature");
+            throw new InvalidDocumentException("Chýba ds:Signature element");
         }
         Attribute dsSignatureId = dsSignature.attribute("Id");
         // TODO: can be blank???
+        // tested on documents 03(valid) and 05(invalid)
         if (dsSignatureId == null || dsSignatureId.getValue().equals("")) {
             throw new InvalidDocumentException("ds:Signature musí mať Id atribút");
         }
@@ -37,12 +38,25 @@ public class JChecker implements Checker{
 
         Namespace xmlnsDs = dsSignature.getNamespaceForPrefix(REQUIRED_NAMESPACE_PREFIX);
         // TODO: required URI check???
+        // tested on documents 03(valid), missing not valid document according to this rule
         if (xmlnsDs == null || !xmlnsDs.getURI().equals(EXPECTED_DS_NAMESPACE_URI)) {
             throw new InvalidDocumentException("ds:Signature musí mať špecifikovaný namespace xmlns:ds");
         }
     }
 
     private void checkDsSignatureValue() throws InvalidDocumentException {
+        Element root = this.document.getRootElement();
+        Element dsSignatureValue = (Element) root.selectSingleNode("//*[name() = 'ds:SignatureValue']");
 
+        if (dsSignatureValue == null) {
+            throw new InvalidDocumentException("Chýba ds:SignatureValue element");
+        }
+
+        Attribute dsSignatureValueId = dsSignatureValue.attribute("Id");
+        // TODO: can be blank???
+        // tested on documents 03(valid) and 06(invalid)
+        if (dsSignatureValueId == null || dsSignatureValueId.getValue().equals("")) {
+           throw new InvalidDocumentException("ds:SignatureValue – musí mať Id atribút");
+        }
     }
 }
