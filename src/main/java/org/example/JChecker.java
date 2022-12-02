9cc355364e2df8454d5e1e3eeca422f7944a86c5
YKB <jakubhlavacka123@gmail.com>
2022-12-02 20:13:55 +0100

    overnie ds:Manifest elementov - ds:Manifest Id, ds:Transforms and ds:DigestMethod

--- src/main/java/org/example/JChecker.java
+++ src/main/java/org/example/JChecker.java
@@ -1,9 +1,8 @@
 package org.example;
 
-import org.dom4j.Attribute;
-import org.dom4j.Document;
-import org.dom4j.Element;
-import org.dom4j.Namespace;
+import org.dom4j.*;
+
+import java.util.*;
 
 public class JChecker implements Checker{
 
@@ -17,6 +16,7 @@ public class JChecker implements Checker{
     public void startCheck() throws InvalidDocumentException {
         checkDsSignature();
         checkDsSignatureValue();
+        checkDsManifestElements();
     }
 
     // specifikacia XADESU strana 17
@@ -56,6 +56,95 @@ public class JChecker implements Checker{
         // tested on documents 03(valid) and 06(invalid)
         if (dsSignatureValueId == null || dsSignatureValueId.getValue().equals("")) {
            throw new InvalidDocumentException("ds:SignatureValue – musí mať Id atribút");
+        }
+    }
+
+    // specifikacia strana 25
+    private void checkDsManifestElements() throws InvalidDocumentException {
+        String CANONICALIZATION_URI = "http://www.w3.org/TR/2001/REC-xml-c14n-20010315";
+        String BASE64_URI = "http://www.w3.org/2000/09/xmldsig#base64 ";
+
+        ArrayList<String> validTransformAlgorithms = new ArrayList<>(2);
+        validTransformAlgorithms.add(CANONICALIZATION_URI);
+        validTransformAlgorithms.add(BASE64_URI);
+
+        Element root =  this.document.getRootElement();
+        List<Node> manifestNodes = root.selectNodes("//*[name() = 'ds:Manifest']");
+
+        for (Node manifestNode : manifestNodes) {
+            // check if manifest has Id attribute
+            Element manifest = (Element) manifestNode;
+            Attribute manifestId = manifest.attribute("Id");
+
+            if (manifestId == null || manifestId.getValue().equals("")) {
+                throw new InvalidDocumentException("každý ds:Manifest element musí mať Id atribút");
+            }
+
+            checkDsTransforms(manifest);
+            checkDsDigestMethod(manifest);
+            // TODO: overenie hodnoty Type atribútu voči profilu XAdES_ZEP - nie je ako atribut v ds:Manifest ale v ds:Manifest > ds:Reference
+        }
+    }
+
+    // specifikacia strana 26
+    private void checkDsTransforms(Element manifest) throws InvalidDocumentException {
+        ArrayList<String> validTransformAlgorithms = new ArrayList<>(2);
+        validTransformAlgorithms.add("http://www.w3.org/TR/2001/REC-xml-c14n-20010315");
+        validTransformAlgorithms.add("http://www.w3.org/2000/09/xmldsig#base64");
+
+        // check if all ds:Transform elements in ds:Transforms element contains valid transformation algorithm
+        Element dsTransforms = (Element) manifest.selectSingleNode("//*[name() = 'ds:Transforms']");
+
+        if (dsTransforms == null) {
+            throw new InvalidDocumentException("ds:Transforms musí byť z množiny podporovaných algoritmov pre" +
+                    " daný element podľa profilu XAdES_ZEP (ds:Manifest neobsahuje ds:Transforms)");
+        }
+
+        for (Iterator<Element> it = dsTransforms.elementIterator("//*[name() = 'ds:Transform']"); it.hasNext();) {
+            Element dsTransform = it.next();
+            Attribute dsTransformAlgorithm = dsTransform.attribute("Algorithm");
+
+            if (dsTransformAlgorithm == null) {
+                throw new InvalidDocumentException("ds:Transforms musí byť z množiny podporovaných algoritmov pre" +
+                        " daný element podľa profilu XAdES_ZEP (nema Algorithm atribut)");
+            }
+
+            String dsTransformAlgorithmValue = dsTransformAlgorithm.getValue();
+            if (!validTransformAlgorithms.contains(dsTransformAlgorithmValue)) {
+                throw new InvalidDocumentException("ds:Transforms musí byť z množiny podporovaných algoritmov pre" +
+                        " daný element podľa profilu XAdES_ZEP");
+            }
+        }
+    }
+
+    // specifikacia strana 27 a 37
+    private void checkDsDigestMethod(Element manifest) throws InvalidDocumentException {
+        String[] validDigestMethodAlgorithms = {
+                "http://www.w3.org/2000/09/xmldsig#sha1",
+                "http://www.w3.org/2001/04/xmldsig-more#sha224",
+                "https://www.w3.org/2001/04/xmldsig-more#sha384",
+                "http://www.w3.org/2001/04/xmldsig-more#sha384",
+                "http://www.w3.org/2001/04/xmlenc#sha512"
+        };
+        List<String> validDigestMethodAlgsList = Arrays.asList(validDigestMethodAlgorithms);
+
+        Element dsDigestMethod = (Element) manifest.selectSingleNode("//*[name() = 'ds:DigestMethod']");
+        if (dsDigestMethod == null) {
+            throw new InvalidDocumentException("ds:DigestMethod – musí obsahovať URI niektorého z podporovaných" +
+                    " algoritmov podľa profilu XAdES_ZEP (chyba ds:DigestMethod)");
+        }
+
+        Attribute dsDigestMethodAlgorithm =  dsDigestMethod.attribute("Algorithm");
+
+        if (dsDigestMethodAlgorithm == null) {
+            throw new InvalidDocumentException("ds:DigestMethod – musí obsahovať URI niektorého z podporovaných" +
+                    " algoritmov podľa profilu XAdES_ZEP (chyba Algorithm attribut)");
+        }
+
+        String dsDigestMethodAlgorithmValue = dsDigestMethodAlgorithm.getValue();
+        if (!validDigestMethodAlgsList.contains(dsDigestMethodAlgorithmValue)) {
+            throw new InvalidDocumentException("ds:DigestMethod – musí obsahovať URI niektorého z podporovaných" +
+                    " algoritmov podľa profilu XAdES_ZEP");
         }
     }
 }
