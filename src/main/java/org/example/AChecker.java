6e93ab7e226ff32bc96a0129f2e4eecd34e6712f
asipka <adam_sipka@tempest.sk>
2022-12-03 18:21:43 +0100

    overenie platnosti casovej peciatky

--- src/main/java/org/example/AChecker.java
+++ src/main/java/org/example/AChecker.java
@@ -1,19 +1,16 @@
 package org.example;
 
+import org.bouncycastle.cms.CMSException;
+import org.bouncycastle.cms.CMSSignedData;
+import org.bouncycastle.tsp.TimeStampToken;
 import org.dom4j.Attribute;
 import org.dom4j.Document;
 import org.dom4j.Element;
 import org.dom4j.Node;
 
 import java.io.ByteArrayInputStream;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
-import java.security.cert.CertificateParsingException;
-import java.security.cert.X509Certificate;
+import java.security.cert.*;
 import java.util.Base64;
-import java.util.Collection;
 import java.util.List;
 
 public class AChecker implements Checker{
@@ -23,6 +20,7 @@ public class AChecker implements Checker{
     final String ERR_MSG_02 = "obidva ds:SignatureProperty musia mať atribút Target nastavený na ds:Signature";
     final String ERR_MSG_03 = "ds:KeyInfo musí obsahovať ds:X509Data, ktorý obsahuje elementy: ds:X509Certificate, ds:X509IssuerSerial, ds:X509SubjectName";
     final String ERR_MSG_04 = "hodnoty elementov ds:X509IssuerSerial a ds:X509SubjectName nesúhlasia s príslušnými hodnatami v certifikáte, ktorý sa nachádza v ds:X509Certificate";
+    final String ERR_MSG_05 = "podpisový certifikát dokumentu nie je platný voči času T z časovej pečiatky ";
 
     public AChecker(Document document) {
         this.document = document;
@@ -32,8 +30,11 @@ public class AChecker implements Checker{
     public void startCheck() throws InvalidDocumentException {
         checkKeyInfo();
         checkSignatureProperties();
+        checkReferencedElemsAndDigestVal();
+        checkCertificateValidity();
     }
 
+    // overenie obsahu ds:KeyInfo
     private void checkKeyInfo() throws InvalidDocumentException {
         Element root = this.document.getRootElement();
         Element dsKeyInfo = getElementByParent(root, "ds:KeyInfo");
@@ -67,6 +68,8 @@ public class AChecker implements Checker{
         }
     }
 
+
+    // overenie obsahu ds:SignatureProperties
     private void checkSignatureProperties() throws InvalidDocumentException {
         Element root = this.document.getRootElement();
         Element dsSignatureProperties = getElementByParent(root, "ds:SignatureProperties");
@@ -94,6 +97,31 @@ public class AChecker implements Checker{
         }
     }
 
+
+    // dereferencovanie URI, kanonikalizácia referencovaných ds:Manifest elementov a overenie hodnôt odtlačkov ds:DigestValue
+    private void checkReferencedElemsAndDigestVal() throws InvalidDocumentException {
+
+    }
+
+
+    // overenie platnosti podpisového certifikátu dokumentu voči času T z časovej pečiatky a voči platnému poslednému CRL
+    private void checkCertificateValidity() throws InvalidDocumentException {
+        X509Certificate cert;
+        Element root = this.document.getRootElement();
+        Element dsX509Certificate = getElementByParent(root, "ds:X509Certificate");
+
+        try {
+            byte[] decoded = Base64.getDecoder().decode(dsX509Certificate.getText().getBytes());
+            cert = (X509Certificate) CertificateFactory.getInstance("X.509").generateCertificate(new ByteArrayInputStream(decoded));
+
+            TimeStampToken timestamp = loadTimeStampToken();
+            cert.checkValidity(timestamp.getTimeStampInfo().getGenTime());
+
+        } catch (CertificateException e) {
+            throw new InvalidDocumentException(ERR_MSG_05);
+        }
+    }
+
     private Element getElementByParent(Element parent, String elemName) throws InvalidDocumentException {
         Element element = (Element) parent.selectSingleNode("//*[name() = '" + elemName + "']");
         if (element == null) {
@@ -114,5 +142,15 @@ public class AChecker implements Checker{
         issuerName2 = issuerName2.replace("ST", "S");
 
         return issuerName1.equals(issuerName2);
+    }
+
+    private TimeStampToken loadTimeStampToken() {
+        try {
+            Element root = this.document.getRootElement();
+            Element timestamp = getElementByParent(root, "xades:EncapsulatedTimeStamp");
+            return new TimeStampToken(new CMSSignedData(Base64.getDecoder().decode(timestamp.getText())));
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }
